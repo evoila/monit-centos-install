e401300e99238b1c9db98123b6f99c002ddf0038
Sebastian Diehl <mail@diehl-s.de>
2017-10-04 14:47:45 +0200

    no message

--- monit-template.sh
+++ monit-template.sh
@@ -4,8 +4,8 @@ export MONIT_PASSWORD=e"voila"
 export CHECK_PATH="/etc/monit/"
 # checks if service is installed
 if [ -a $CHECK_PATH* ]; then
-
-else
+    wget $REPOSITORY_MONIT/monit-run.sh
+    else
     wget $REPOSITORY_MONIT/monit-install.sh
     chmod +x monit-install.sh
     ./monit-install.sh
@@ -13,10 +13,7 @@ else
     wget $REPOSITORY_MONIT/monit-configuration.sh
     chmod +x monit-configuration.sh
     ./monit-configuration.sh
-   
-
 fi
     # loads and executes script for startup of monit
-    wget $REPOSITORY_MONIT/monit-run.sh
     chmod +x monit-run.sh
     ./monit-run.sh monit
